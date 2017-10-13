139bbc7140230293370983845cd23c4910e195f3
DESKTOP-2IO1N0U\Stefanos <stefkaps@utexas.edu>
2017-10-12 22:09:08 -0500

    Added LDA 1-5 + some insight comments

--- Project 2.R
+++ Project 2.R
@@ -54,6 +54,7 @@ mean(glm.pred==status2.testing)
 ## LDA
 ##
 
+#LDA1 - hz predictor + 1 jitter predictor
 lda.fit=lda(status~mdvp_fo_hz+mdvp_jitter,data=df,subset=training)
 lda.fit
 plot(lda.fit)
@@ -67,6 +68,67 @@ table(lda.pred$class,testing$status)
 table(lda.pred$class==testing$status)
 mean(lda.pred$class==testing$status)
 table(lda.pred$class!=testing$status)
+
+# LDA2 - just all jitter predictors
+lda2.fit=lda(status~mdvp_jitter+mdvp_rap+mdvp_ppq+jitter_ddp,data=df,subset=training)
+lda2.fit
+plot(lda2.fit)
+
+lda2.pred=predict(lda2.fit, testing)
+lda2_df = data.frame(lda2.pred)
+#View(lda.pred)
+ggplot(lda2_df) + geom_histogram(mapping=aes(x=LD1))
+ggplot(lda2_df) + geom_boxplot(mapping = aes(x=class,y=LD1))
+table(lda2.pred$class,testing$status)
+table(lda2.pred$class==testing$status) #no real falses because everyone with jitters has PD -> jitters is "perfect" predictor of PD
+mean(lda2.pred$class==testing$status)
+table(lda2.pred$class!=testing$status)
+
+##LDA3 - just all shimmer predictors
+##  LDA3 vs LDA2 - note people without PD have shimmers (seen in LDA3) but not jitters (seen in LDA2)
+lda3.fit=lda(status~mdvp_shimmer+mdvp_shimmer_db+shimmer_apq3+shimmer_apq5+mdvp_apq+shimmer_dda,data=df,subset=training)
+lda3.fit
+plot(lda3.fit)
+
+lda3.pred=predict(lda3.fit, testing)
+lda3_df = data.frame(lda3.pred)
+#View(lda.pred)
+ggplot(lda3_df) + geom_histogram(mapping=aes(x=LD1))
+ggplot(lda3_df) + geom_boxplot(mapping = aes(x=class,y=LD1))
+table(lda3.pred$class,testing$status)
+table(lda3.pred$class==testing$status)
+mean(lda3.pred$class==testing$status)
+table(lda3.pred$class!=testing$status)
+
+## LDA4 - all shimmer predictors + all jitter predictors
+lda4.fit=lda(status~mdvp_shimmer+mdvp_shimmer_db+shimmer_apq3+shimmer_apq5+mdvp_apq+shimmer_dda+mdvp_jitter+mdvp_rap+mdvp_ppq+jitter_ddp,data=df,subset=training)
+lda4.fit
+plot(lda4.fit)
+
+lda4.pred=predict(lda4.fit, testing)
+lda4_df = data.frame(lda4.pred)
+#View(lda.pred)
+ggplot(lda4_df) + geom_histogram(mapping=aes(x=LD1))
+ggplot(lda4_df) + geom_boxplot(mapping = aes(x=class,y=LD1))
+table(lda4.pred$class,testing$status)
+table(lda4.pred$class==testing$status)
+mean(lda4.pred$class==testing$status)
+table(lda4.pred$class!=testing$status)
+
+## LDA5 - all shimmer predictors + hertz predictor
+lda5.fit=lda(status~mdvp_fo_hz+mdvp_shimmer+mdvp_shimmer_db+shimmer_apq3+shimmer_apq5+mdvp_apq+shimmer_dda,data=df,subset=training)
+lda5.fit
+plot(lda5.fit) #compare LDA5 vs LDA3 (all shimmer) -> shimmer and hertz are not as good predictors of someone having PD as jitters
+
+lda5.pred=predict(lda5.fit, testing)
+lda5_df = data.frame(lda5.pred)
+#View(lda.pred)
+ggplot(lda5_df) + geom_histogram(mapping=aes(x=LD1))
+ggplot(lda5_df) + geom_boxplot(mapping = aes(x=class,y=LD1))
+table(lda5.pred$class,testing$status)
+table(lda5.pred$class==testing$status) #hertz predictor is a better predictor of getting real false's
+mean(lda5.pred$class==testing$status)
+table(lda5.pred$class!=testing$status)
 
 
 # df1 = dplyr::bind_cols(testing,lda_df)
